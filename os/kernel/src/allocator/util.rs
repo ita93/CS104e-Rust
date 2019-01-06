58af7aa6fe65def598faa24eaddd72167922d1bf
Nguyen Dinh Phi <phind.uet@gmail.com>
2019-01-06 15:52:41 +0700

    Align up and Align down

--- os/kernel/src/allocator/util.rs
+++ os/kernel/src/allocator/util.rs
@@ -6,7 +6,13 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    if align != 0 && ( align & (align - 1) == 0) {
+        //align is power of 2
+        let mut res:usize = addr / align;
+        res * align
+    } else {
+        panic!("align must be power of 2");
+    }
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -17,5 +23,16 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    if align != 0 && ( align & (align - 1) == 0) {
+        //align is power of 2
+        let mut res:usize = addr / align;
+        let remainer = addr % align;
+        if remainer > 0 {
+            (res + 1) * align
+        } else {
+            res * align
+        }
+    } else {
+        panic!("align must be power of 2");
+    }
 }
